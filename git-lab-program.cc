a83eb6611d6ea6bc1bdd6c7eb3caacad1ee13f40
Delonte Roberts <156972645+yeahlonte@users.noreply.github.com>
2024-01-24 19:57:53 -0500

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Delonte Roberts 
+ *        Date: 1/24/2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;
+  return 0;
+}// main
